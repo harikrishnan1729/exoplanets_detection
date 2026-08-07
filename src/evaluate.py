8213fd310ee26f91a8aaf13758885207bd4ffc11
harikrishnan1729 <hariarjun1729@gmail.com>
2026-08-07 19:28:12 +0530

    f1: 0.2222222

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,6 +3,8 @@ from dataset import load_data
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.metrics import roc_auc_score
 import numpy as np
+from sklearn.metrics import f1_score
+
 
 # Load the TEST dataset
 _, _, X_test, _, _, y_test = load_data()
@@ -14,13 +16,28 @@ model = tf.keras.models.load_model("models/exoplanet_detector.keras")
 y_pred_prob = model.predict(X_test, verbose=0).flatten()
 
 # Thresholds to test
-threshold = .5
+threshold = .7
+
+thresholds = np.arange(0.01, 1.00, 0.01)
+
+best_threshold = 0
+best_f1 = 0
+
+
+for t in thresholds:
+    y_pred = (y_pred_prob >= t).astype(int)
+    f1 = f1_score(y_test, y_pred)
+
+    if f1 > best_f1:
+        best_f1 = f1
+        best_threshold = t
+
 
 
 # Convert probabilities to binary predictions
-y_pred = (y_pred_prob >= threshold).astype(int)
+y_pred = (y_pred_prob >= best_threshold).astype(int)
 print("=" * 60)
-print(f"Threshold = {threshold:.2f}")
+print(f"Threshold = {best_threshold:.2f} f1: {best_f1}")
 print("=" * 60)
 print("\nConfusion Matrix")
 print(confusion_matrix(y_test, y_pred))
